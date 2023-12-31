d4ee21ea59dba1438b9a5d3ed1c5e4e39e355f9b
Shu12388y <spl.sp999@gmail.com>
2023-12-31 20:53:24 +0530

    'update'

--- next_project_05/app/page.tsx
+++ next_project_05/app/page.tsx
@@ -1,9 +1,38 @@
-import Image from 'next/image'
+"use client"
+
+
+import React, { useState } from "react";
 
 export default function Home() {
+  // define a state
+  const [counter, setCounter] = useState(15);
+
+  // upcounter function
+  function upCounter() {
+    // use the updater function for state to ensure correct updates
+    setCounter((prevCounter) => prevCounter + 1);
+  }
+
+  // downcounter function
+  function downCounter() {
+    // use the updater function for state to ensure correct updates
+    setCounter((prevCounter) => prevCounter - 1);
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     
-    </main>
-  )
+    <>
+      <div className="flex flex-col justify-center items-center mt-10">
+        <h2 className="mt-4">Counter</h2>
+        <h2>{counter}</h2>
+        <div className="flex items-center gap-4 ">
+          <button onClick={upCounter} className="bg-yellow-400 p-2 mt-2 text-black-100">
+            Up value
+          </button>
+          <button onClick={downCounter} className="bg-yellow-400 p-2 mt-2 text-black-100">
+            Down value
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }
