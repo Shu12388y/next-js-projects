80bef9924d8804752b39840d34e0bd91bdab6e4c
Shu12388y <spl.sp999@gmail.com>
2024-01-02 17:16:09 +0530

    'update'

---
+++ next_project_07/app/page.tsx
@@ -0,0 +1,16 @@
+import { Context } from "react"
+
+// nvim
+// y copy
+// p paste
+// shift + v select line
+// d delete 
+export default function Home() {
+  return (
+   <>
+    <h1>Home</h1>
+    
+    
+    </>
+  )
+}
